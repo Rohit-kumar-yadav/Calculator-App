e160c8e33be967f335d865acb31b607d65793b41
Rohit kumar yadav <ry256423@gmail.com>
2021-09-20 15:51:52 +0530

    Added sub. ,mul. , div. button and also program

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -10,9 +10,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText value1,value2;
-    Button btn;
+    Button add;
+    Button sub;
+    Button mul;
+    Button div;
     TextView total;
-    int sum;
+    double sum;
 
    @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +24,58 @@ public class MainActivity extends AppCompatActivity {
 
         value1 = findViewById(R.id.edit1);
         value2 = findViewById(R.id.edit2);
-        btn = findViewById(R.id.btn);
+        add = findViewById(R.id.btn);
         total = findViewById(R.id.text);
-
-        btn.setOnClickListener(new View.OnClickListener() {
+        sub = findViewById(R.id.btn2);
+        mul = findViewById(R.id.btn1);
+        div = findViewById(R.id.btn3);
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String num1 = value1.getText().toString();
-                String num2 = value2.getText().toString();
+                double num1 = Double.parseDouble(value1.getText().toString());
+                double num2 = Double.parseDouble(value2.getText().toString());
 
-                sum = Integer.parseInt(num1)+Integer.parseInt(num2);
+                sum = num1+num2;
 
-                total.setText(Integer.toString(sum));
+                total.setText(String.valueOf(sum));
             }
+
         });
+       sub.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               double num1 = Double.parseDouble(value1.getText().toString());
+               double num2 = Double.parseDouble(value2.getText().toString());
 
+               sum = num1 - num2;
 
+               total.setText(String.valueOf(sum));
 
+           }
+       });
+       mul.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               double num1 = Double.parseDouble(value1.getText().toString());
+               double num2 = Double.parseDouble(value2.getText().toString());
 
+               sum = num1 * num2;
+
+               total.setText(String.valueOf(sum));
+
+           }
+       });
+       div.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               double num1 = Double.parseDouble(value1.getText().toString());
+               double num2 = Double.parseDouble(value2.getText().toString());
+
+               sum = num1 / num2;
+
+               total.setText(String.valueOf(sum));
+           }
+       });
 
     }
 }
